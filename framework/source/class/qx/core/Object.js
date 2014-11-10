e81ae41ac3f04b3bdaae5eb61971c8c767f4c126
Dmitry Golubkov <master@hsdesign.ru>
2014-11-10 16:42:01 +0300

    Fix memory leak of objects which removed from layout & disposed. These objects kept in memory.

--- framework/source/class/qx/core/Object.js
+++ framework/source/class/qx/core/Object.js
@@ -422,7 +422,13 @@ qx.Class.define("qx.core.Object",
         qx.event.Registration.deleteAllListeners(this);
       }
     }
-
+    // Kludge - cleanup evil static queues
+    if (typeof qx.Class.getByName("qx.ui.core.queue.Visibility") !== "undefined") {
+      qx.ui.core.queue.Visibility.remove(this);
+    }
+    if (typeof qx.Class.getByName("qx.html.Element") !== "undefined") {
+      delete qx.html.Element._modified[this.$$hash];
+    }
     // Cleanup object registry
     qx.core.ObjectRegistry.unregister(this);
 
